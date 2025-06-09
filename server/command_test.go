13866126e3538f6ecdbf0b73fbb7ca89dc40604a
polinapianina <139022280+polinapianina@users.noreply.github.com>
2025-06-09 04:41:52 -0400

    Add initial unit tests for Command.go default and unknown-command handlers (#148)

---
+++ server/command_test.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/mattermost/mattermost/server/public/model"
+	"github.com/mattermost/mattermost/server/public/plugin/plugintest"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+
+	"github.com/mattermost/mattermost-plugin-confluence/server/config"
+)
+
+func setupMockAPI(t *testing.T) *plugintest.API {
+	mockAPI := &plugintest.API{}
+	config.Mattermost = mockAPI
+	mockAPI.On("GetUser", mock.AnythingOfType("string")).Return(&model.User{Roles: "system_user"}, nil)
+	return mockAPI
+}
+
+func TestExecuteConfluenceDefault(t *testing.T) {
+	mockAPI := setupMockAPI(t)
+
+	cmdArgs := &model.CommandArgs{UserId: "U1", ChannelId: "C1"}
+	resp := executeConfluenceDefault(nil, cmdArgs)
+
+	assert.Equal(t, model.CommandResponseTypeEphemeral, resp.ResponseType)
+	assert.True(t, strings.HasPrefix(resp.Text, invalidCommand))
+	assert.Contains(t, resp.Text, commonHelpText)
+
+	mockAPI.AssertExpectations(t)
+}
+
+func TestHandler_Handle_Default(t *testing.T) {
+	mockAPI := setupMockAPI(t)
+
+	p := &Plugin{}
+	cmdArgs := &model.CommandArgs{UserId: "U1", ChannelId: "C1"}
+	resp := ConfluenceCommandHandler.Handle(p, cmdArgs, "foo")
+
+	assert.Equal(t, model.CommandResponseTypeEphemeral, resp.ResponseType)
+	assert.Contains(t, resp.Text, invalidCommand)
+
+	mockAPI.AssertExpectations(t)
+}
